1fd32811174f31f4fa3d8bc34506ff29bd106422
maBottnn14 Hhs <46973490+MaBottnn14@users.noreply.github.com>
2019-02-14 10:24:12 -0500

    Updated from Brython Server: 2/14/2019 10:24:11 AM

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,7 +1,7 @@
 """
 multiplication-table.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: maBottnn14
+Credit: tristan taught me how to use the range function
 Assignment:
 
 Write and submit a Python program that prints a multiplication table. The user 
@@ -21,3 +21,11 @@ Height of multiplication table: 8
   7  14  21  28  35  42  49  56  63  70
   8  16  24  32  40  48  56  64  72  80
 """
+
+width = int(input("Width of multiplication table: "))
+height = int(input("Height of multiplication table: "))
+
+for M in range(1, 1 + width) :
+    print("")
+    for B in range(1, 1 + height) :
+        print(M*B , end=(" "))
